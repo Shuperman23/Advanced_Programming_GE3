f50002e49c3a742b3195e52ee3e08a046f998e60
robinchoco <rruizs205@gmail.com>
2024-06-29 03:56:15 -0600

    Creacion de AutomapperProfile

---
+++ ExamenCGastos/ExamenCGastos/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ExamenCGastos.DTOs;
+using ExamenCGastos.Models;
+
+namespace ExamenCGastos
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        {
+            CreateMap<Proveedor, ProveedorDto>().ReverseMap();
+            CreateMap<Producto, ProductoDto>().ReverseMap();
+            CreateMap<Inventario, InventarioDto>().ReverseMap();
+            CreateMap<Usuario, UsuarioDTO>().ReverseMap();
+        }
+    }
+}
