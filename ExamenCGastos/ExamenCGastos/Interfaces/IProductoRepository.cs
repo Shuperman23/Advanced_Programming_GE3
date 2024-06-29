b8b080dbc409c3b67b969e6695d97da5bbdbb40b
ronny ruiz <roniruiz23@hotmail.com>
2024-06-29 03:37:30 -0600

    ID

--- ExamenCGastos/ExamenCGastos/Interfaces/IProductoRepository.cs
+++ ExamenCGastos/ExamenCGastos/Interfaces/IProductoRepository.cs
@@ -6,7 +6,7 @@ namespace ExamenCGastos.Interfaces
     public interface IProductoRepository : IBaseRepository<Producto>
     {
         Task<List<Producto>> GetProductosAsync();
-        Task<Producto?> GetProductoById(int productoId);
+        Task<Producto?> GetProductoById(int Id);
         Task<StoredProcedureDto?> CreateNewProductoAsync(ProductoDto resource);
         Task<StoredProcedureDto?> UpdateProductoAsync(ProductoDto resource);
     }
